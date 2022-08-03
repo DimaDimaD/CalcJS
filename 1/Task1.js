3d0ce38d030250ed11e66cbef111f82b625c31e5
Dima <dimaecs@gmail.com>
2022-08-03 16:46:05 +0300

    Задача 1, ДЗ 1

--- 1/Task1.js
+++ 1/Task1.js
@@ -1,1 +1,11 @@
-console.log('Hi');
+let a = prompt('Введите число:');
+let b = prompt('Введите систему исчисления:');
+
+function numToBase(a, b) {
+
+    let num = Number(a);
+    let base = Number(b);
+
+    return num && base && base > 1 && base < 37 ?
+           num.toString(base) : 'Некорректный ввод';
+}
