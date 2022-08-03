a0ff36682c026bfc1954e198a646e9dfb7cab25d
Dima <dimaecs@gmail.com>
2022-08-03 18:51:21 +0300

    Задача 2 с файлом html

--- 1/Task2/Task2.js
+++ 1/Task2/Task2.js
@@ -1,3 +1,11 @@
 let a = prompt('Введите первое число:');
 let b = prompt('Введите второе число:');
 
+function sumAndDif(a, b) {
+    
+    return !Number(a) ? 'Некорректный ввод' :
+            !Number(b) || Number(b) == 0 ? 'Некорректный ввод' : 
+                `Ответ: ${Number(a) + Number(b)}, ${Number(a) / Number(b)}`;
+}
+
+console.log(sumAndDif(a, b));
